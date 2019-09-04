ceef14c5c315c74fb21e9782d546b60353f67be4
Pineapple <34860978+XueGui@users.noreply.github.com>
2019-09-04 15:54:13 +0800

    Create MinCostMaxFlow.cpp

---
+++ graph/MinCostMaxFlow.cpp
@@ -0,0 +1,110 @@
+//1
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+
+const int N = 5e3 + 5, M = 1e5 + 5;
+const int INF = 0x3f3f3f3f;
+int n, m, tot = 1, lnk[N], cur[N], ter[M], nxt[M], cap[M], cost[M], dis[N], ret;
+bool vis[N];
+
+void add(int u, int v, int w, int c) {
+  ter[++tot] = v, nxt[tot] = lnk[u], lnk[u] = tot, cap[tot] = w, cost[tot] = c;
+}
+void addedge(int u, int v, int w, int c) { add(u, v, w, c), add(v, u, 0, -c); }
+bool spfa(int s, int t) {
+  memset(dis, 0x3f, sizeof(dis));
+  memcpy(cur, lnk, sizeof(lnk));
+  std::queue<int> q;
+  q.push(s), dis[s] = 0, vis[s] = 1;
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop(), vis[u] = 0;
+    for (int i = lnk[u]; i; i = nxt[i]) {
+      int v = ter[i];
+      if (cap[i] && dis[v] > dis[u] + cost[i]) {
+        dis[v] = dis[u] + cost[i];
+        if (!vis[v]) q.push(v), vis[v] = 1;
+      }
+    }
+  }
+  return dis[t] != INF;
+}
+int dfs(int u, int t, int flow) {
+  if (u == t) return flow;
+  vis[u] = 1;
+  int ans = 0;
+  for (int &i = cur[u]; i && ans < flow; i = nxt[i]) {
+    int v = ter[i];
+    if (!vis[v] && cap[i] && dis[v] == dis[u] + cost[i]) {
+      int x = dfs(v, t, std::min(cap[i], flow - ans));
+      if (x) ret += x * cost[i], cap[i] -= x, cap[i ^ 1] += x, ans += x;
+    }
+  }
+  vis[u] = 0;
+  return ans;
+}
+int mcmf(int s, int t) {
+  int ans = 0;
+  while (spfa(s, t)) {
+    int x;
+    while ((x = dfs(s, t, INF))) ans += x;
+  }
+  return ans;
+}
+int main() {
+  int s, t;
+  scanf("%d%d%d%d", &n, &m, &s, &t);
+  while (m--) {
+    int u, v, w, c;
+    scanf("%d%d%d%d", &u, &v, &w, &c);
+    addedge(u, v, w, c);
+  }
+  int ans = mcmf(s, t);
+  printf("%d %d\n", ans, ret);
+  return 0;
+}
+
+//2
+const int inf= ~0U>>1;
+template<class U,class V>
+struct MCMF{
+    static const int N = 6000, M = 201010;
+    int h[N], ing[N], pre[N], to[M], ne[M], e, s, t, n;
+    U cap[M]; V dis[N], cost[M];
+    void ini(int _n = N){ fill(h , h + (n=_n) , -1);e = 0;}
+    void liu(int u,int v,U c,V w){ to[e] = v;ne[e] = h[u];cap[e] = c;cost[e] = w;h[u] = e++;}
+    void link(int u,int v,U c,V w){ liu(u,v,c,w);liu(v,u,0,-w); }
+    bool spfa(){
+        queue<int> Q;
+        fill(dis, dis+n, inf);
+        Q.push(s), ing[s] = 1, dis[s] = 0;
+        while(!Q.empty()){
+            int c = Q.front(); Q.pop(); ing[c] = 0;
+            for(int k = h[c]; ~k; k = ne[k]) if (cap[k] > 0) {
+                int v = to[k];
+                if(dis[c] + cost[k] < dis[v]){
+                    dis[v] = dis[c] + cost[k];
+                    pre[v] = k;
+                    if(!ing[v]) Q.push(v) , ing[v] = 1;
+                }
+            }
+        }
+        return dis[t] != inf;
+    }
+    U flow; V mincost;
+    pair<U, V> run(int _s,int _t){
+        s = _s , t = _t;
+        flow = mincost = 0;
+        while(spfa()){
+            U pl = inf; int p, k;
+            for(p = t;p != s;p = to[k^1]) pl = min(pl, cap[k = pre[p]]);
+            for(p = t;p != s;p = to[k^1]) cap[k = pre[p]] -= pl, cap[k^1] += pl;
+			mincost += pl * dis[t];
+            flow += pl;
+        }
+        return make_pair(flow, mincost);
+    }
+};
+MCMF<int,int> flow;
