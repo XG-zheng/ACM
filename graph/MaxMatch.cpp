52be5babd998ab8119ed44b1a801dec0cafbb967
KC <2444521156@qq.com>
2020-05-03 12:14:57 +0800

    upd1

--- graph/MaxMatch.cpp
+++ graph/MaxMatch.cpp
@@ -1,6 +1,12 @@
 //n  the number of vertex in left part   [1,n]
 // m  the number of vertex in right part [1,m]
 // edge (left)u->v(right) 
+
+//最大独立集输出方案 |最大独立集| = |V| - 最大匹配 
+//先求最大匹配，每一对匹配的点只能选取左边或者右边，不在匹配上的点则要全选。 
+//则从不在匹配上的左半图点开始搜索，能走到的左半图的点都可选，右半图走到的则不可选。 
+//存在某对匹配左右都访问不到，那么默认选取了左边的点。
+
 struct MaxMatch{
 	const static int N = 1010;
 	int link[N];
