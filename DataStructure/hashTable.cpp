7a35f29574af27a626c8c6cb4bd07a63718eb5fa
KC <2444521156@qq.com>
2020-05-27 20:53:43 +0800

    upd

--- DataStructure/hashTable.cpp
+++ DataStructure/hashTable.cpp
@@ -1,17 +1,15 @@
 struct hashTable{
-	static const int N = 1e5 + 5;
+	static const int N = 5e5 + 5;
 	int h[N],ne[N],sta[N],cnt;
 	ll v[N];
 	void clear(){
 		memset(h,-1,sizeof(h));
-		rep(i , 0  , N)h[i] = -1 , sta[i] = -1,ne[i] = -1,v[i] = 0;
 		cnt = 0;
-	} 
+	}
 	ll &operator[](int cur){
 		int i;
-		for(i = h[cur%N] ;(~i) && sta[i] != cur ; i = ne[i]);
-		if(i == -1)sta[cnt] = cur,ne[cnt] = h[cur%N],h[cur%N] = i = cnt++;
+		for(i = h[cur%N];(~i) && sta[i] != cur;i = ne[i]);
+		if(i == -1)sta[cnt] = cur,ne[cnt] = h[cur%N],h[cur%N] = i = cnt++,v[i] = 0;
 		return v[i];
 	}
-	
 }f[2];
